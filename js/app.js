374568dd22f68778961d217fe7a25666ee755b45
linc31177 <95111321+linc31177@users.noreply.github.com>
2021-11-27 15:48:39 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/20211127WebAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/20211127WebAR1.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
